42be53af38161b7dcd870e495cfeae72111c3b99
suranjanachary <101392637+suranjanachary@users.noreply.github.com>
2022-11-10 23:10:29 +0530

    day3

--- FLASH-GO/src/main/java/com/masai/model/Customer.java
+++ FLASH-GO/src/main/java/com/masai/model/Customer.java
@@ -1,15 +1,24 @@
 package com.masai.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Customer extends AbstractUser {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer customerId;
+	
+	
+//	List<TripBooking> tripList =  new ArrayList<>();
 	
 	public Customer() {
 		// TODO Auto-generated constructor stub
