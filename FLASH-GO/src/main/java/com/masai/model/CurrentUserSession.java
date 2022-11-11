734e5e33388c3b70ad8ca266714c2d90c020f4a4
shubhamughade <shubhamughade21@gmail.com>
2022-11-11 17:25:46 +0530

    added

--- FLASH-GO/src/main/java/com/masai/model/CurrentUserSession.java
+++ FLASH-GO/src/main/java/com/masai/model/CurrentUserSession.java
@@ -1,5 +1,29 @@
 package com.masai.model;
 
-public class CurrentUserSession {
+import java.time.LocalDateTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.ToString;
+
+
+@Entity
+@Data
+@ToString
+
+public class CurrentUserSession {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	private Integer userId;
+	
+	private LocalDateTime localDateTime;
+	
+	private String uuid;
 }
--- FLASH-GO/src/main/java/com/masai/model/CustomerLogin.java
+++ FLASH-GO/src/main/java/com/masai/model/CustomerLogin.java
@@ -1,5 +1,22 @@
 package com.masai.model;
 
-public class CustomerLogin {
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
+import lombok.Data;
+import lombok.ToString;
+
+@Entity
+@Data
+@ToString
+public class CustomerLogin {
+	
+	@NotNull
+	@Id
+	private Integer userId;
+	
+	private String customerName;
+	
+	private String password;
 }
