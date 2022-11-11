8b6e26e3fad6360c9d48c24d6b658bdffe4f59f0
suranjanachary <101392637+suranjanachary@users.noreply.github.com>
2022-11-11 22:28:26 +0530

    autowired admin

--- FLASH-GO/src/main/java/com/masai/services/AdminServiceImp.java
+++ FLASH-GO/src/main/java/com/masai/services/AdminServiceImp.java
@@ -2,10 +2,17 @@ package com.masai.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.masai.exception.AdminException;
 import com.masai.model.Admin;
 import com.masai.model.TripBooking;
+import com.masai.repository.AdminDao;
 
 public class AdminServiceImp implements AdminService {
+	
+	@Autowired
+	public AdminDao admindao;
 
 	@Override
 	public Admin insertAdmin(Admin admin) throws AdminException {
